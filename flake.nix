3834934317893bb38858bf69b52506091e836030
Tommy Bidne <tbidne@protonmail.com>
2023-02-22 09:17:23 +1300

    Switch to flake-parts; update nix ghc to 9.4.4

--- flake.nix
+++ flake.nix
@@ -4,49 +4,53 @@
     url = "github:edolstra/flake-compat";
     flake = false;
   };
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs =
-    { flake-compat
-    , flake-utils
+    inputs@{ flake-compat
+    , flake-parts
     , nixpkgs
     , self
     }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      buildTools = c: with c; [
-        cabal-install
-        pkgs.gnumake
-        pkgs.zlib
-      ];
-      devTools = c: with c; [
-        (pkgs.haskell.lib.dontCheck ghcid)
-        (hlib.overrideCabal haskell-language-server (old: {
-          configureFlags = (old.configureFlags or [ ]) ++
-            [
-              "-f -brittany"
-              "-f -floskell"
-              "-f -fourmolu"
-              "-f -stylishhaskell"
-            ];
-        }))
-      ];
-      ghc-version = "ghc924";
-      compiler = pkgs.haskell.packages."${ghc-version}";
-      mkPkg = returnShellEnv:
-        compiler.developPackage {
-          inherit returnShellEnv;
-          name = "env-guard";
-          root = ./.;
-          modifier = drv:
-            pkgs.haskell.lib.addBuildTools drv
-              (buildTools compiler ++
-                (if returnShellEnv then devTools compiler else [ ]));
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      perSystem = { pkgs, ... }:
+        let
+          buildTools = c: with c; [
+            cabal-install
+            pkgs.gnumake
+            pkgs.zlib
+          ];
+          devTools = c: with c; [
+            (pkgs.haskell.lib.dontCheck ghcid)
+            (hlib.overrideCabal haskell-language-server (old: {
+              configureFlags = (old.configureFlags or [ ]) ++
+                [
+                  "-f -brittany"
+                  "-f -floskell"
+                  "-f -fourmolu"
+                  "-f -stylishhaskell"
+                ];
+            }))
+          ];
+          ghc-version = "ghc944";
+          compiler = pkgs.haskell.packages."${ghc-version}";
+          mkPkg = returnShellEnv:
+            compiler.developPackage {
+              inherit returnShellEnv;
+              name = "env-guard";
+              root = ./.;
+              modifier = drv:
+                pkgs.haskell.lib.addBuildTools drv
+                  (buildTools compiler ++
+                    (if returnShellEnv then devTools compiler else [ ]));
+            };
+        in
+        {
+          packages.default = mkPkg false;
+          devShells.default = mkPkg true;
         };
-    in
-    {
-      packages.default = mkPkg false;
-      devShells.default = mkPkg true;
-    });
+      systems = [
+        "x86_64-linux"
+      ];
+    };
 }
